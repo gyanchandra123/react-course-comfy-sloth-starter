a49795c3444aa6bc02f3aecf2acc4bb9ffe67fe1
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-09 16:41:37 +0530

    287. React Router - Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+// these 3 component will be display in every pages.
+import {
+  Home,
+  Products,
+  PrivateRoute,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+
+        <Route path="/products" exact>
+          <Products />
+        </Route>
+
+        <Route path="/products/:id" children={<SingleProduct />} exact>
+          <Products />
+        </Route>
+
+        <Route path="/cart" exact>
+          <Cart />
+        </Route>
+        <Route path="/about" exact>
+          <About />
+        </Route>
+
+        <Route path="/checkout" exact>
+          <Checkout />
+        </Route>
+        <Route path="*" exact>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
