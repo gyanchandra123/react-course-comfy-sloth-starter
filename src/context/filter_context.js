1eb4856d455fd1d8cae73c6db446937fb83b7344
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-10 19:40:41 +0530

    315. Products Page - Grid View

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -31,7 +31,7 @@ export const FilterProvider = ({ children }) => {
   }, [products])
 
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider value={{...state}}>
       {children}
     </FilterContext.Provider>
   )
