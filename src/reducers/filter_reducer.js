56be7cb93ab1186af33d69b55c72e35060c6481f
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-11 10:26:56 +0530

    321. Filters - Default Values

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,10 +11,15 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
+
+    let maxPrice = action.payload.map((p) => p.price)
+    maxPrice = Math.max(...maxPrice)
+
     return {
       ...state,
       all_products: [...action.payload],
       filtered_products: [...action.payload],
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     };
   }
   if (action.type === SET_GRIDVIEW) {
@@ -32,7 +37,7 @@ const filter_reducer = (state, action) => {
     const { sort, filtered_products } = state;
 
     let tempProducts = [...filtered_products];
-    
+
     if (sort === "price-lowest") {
       tempProducts = tempProducts.sort((a, b) => {
         if (a.price < b.price) {
